ed4440fdc956b3aca85a9a653260482021d20b9e
vika_repich <vv.repich@gmail.com>
2020-11-18 23:00:18 +0200

    Variables and types

---
+++ Variables.py
@@ -0,0 +1,21 @@
+a = 50
+
+b = 'abc' + '45'
+
+print (b)
+
+print(b.isalnum())
+print(b.index('c'))
+
+def index(needle, stack):
+    return stack.index(needle)
+
+print('found: ')
+print(index('bc', b))
+
+rrr
+
+
+
+
+
